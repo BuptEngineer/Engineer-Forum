1b76ee7ed3801ff14af681a7aa83e46d11f42c1f
shihunyewu <sgy_qingdao2014@163.com>
2018-07-09 17:08:23 +0800

    spring-mvc 路由调通，实现了路由例子-UserController-user/test.jsp

--- src/com/qust/engineer/controller/UserController.java
+++ src/com/qust/engineer/controller/UserController.java
@@ -1,6 +1,5 @@
 package com.qust.engineer.controller;
 
-import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
@@ -8,16 +7,14 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
-
 @Controller
-@RequestMapping("/user")
+@RequestMapping("/user") // 这里的 user 也需要在 user 文件夹下
 public class UserController {
 
     @RequestMapping("/userShow")
     public String toIndex(HttpServletRequest request, Model model){
 
         model.addAttribute("user", "sgy");
-
         return "test"; // 返回页面可以带有文件夹
     }
 
