93b97c54f469b4695ff452cb84df16b623bc2d19
shihunyewu <sgy_qingdao2014@163.com>
2018-07-09 15:17:42 +0800

    修正Mybatis bug

--- src/com/qust/engineer/controller/UserController.java
+++ src/com/qust/engineer/controller/UserController.java
@@ -1,5 +1,24 @@
 package com.qust.engineer.controller;
 
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+
+@Controller
+@RequestMapping("/user")
 public class UserController {
+
+    @RequestMapping("/userShow")
+    public String toIndex(HttpServletRequest request, Model model){
+
+        model.addAttribute("user", "sgy");
+
+        return "test"; // 返回页面可以带有文件夹
+    }
 
 }
